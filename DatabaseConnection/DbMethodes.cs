2ca1825513b022fe428be3756d5d15d22dcb54f3
Yuriy <63811829+Yu-C-Sharp@users.noreply.github.com>
2020-05-26 21:59:36 +0300

    Added SubProjects inside main

---
+++ DatabaseConnection/DbMethodes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using static System.Console;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ClassLibraryNumber;
+
+namespace DatabaseConnection
+{
+    public class DbMethodes
+    {
+        public void LoadToDb(Number obj)
+        {
+            using (ApplicationContext db = new ApplicationContext(CreateOptions.JSON_FILE_Configuration()))
+            {
+                db.Add(obj);
+                db.SaveChanges();
+            }
+        }
+        public void UploadFromDbLastFiveResults()
+        {
+            using (ApplicationContext db = new ApplicationContext(CreateOptions.JSON_FILE_Configuration()))
+            {
+                /////   Teacher's Solution
+                /* 
+                 var lastFiveResult = db.Numbers.OrderBy(b => b.NumberID).Skip(Math.Max(0, db.Numbers.OrderBy(b => b.NumberID).Count() - 5));
+                 WriteLine("The last five results of the program :");
+                 foreach (Number item in lastFiveResult)
+                     WriteLine($"Id - {item.NumberID} :: Number - {item.Num} : Solution - [{item.Result}]");
+                 if (lastFiveResult.Count() < 5) WriteLine("There are no more results in Database...");
+                 */
+
+                ///// My Solution
+                IEnumerable<Number> query = from item in db.Numbers.OrderByDescending(key => key.NumberID)
+                                            where item.NumberID < 6
+                                            select item;
+                WriteLine("The last five results of the program :");
+                foreach (Number item in query)
+                    WriteLine($"Id - {item.NumberID} :: Number - {item.Num} : Solution - [{item.Result}]");
+                if (query.Count() < 5) WriteLine("There are no more results in Database...");
+            }
+        }
+    }
+}
---
+++ WebVersionIntegralNumber/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebVersionIntegralNumber.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ClassLibraryNumber;
+using DatabaseConnection;
+using ConsoleDataEntryAndProcessing;
+using Newtonsoft.Json;
+using Microsoft.VisualStudio.Web.CodeGeneration.Utils;
+using Microsoft.AspNetCore.Http;
+
+
+namespace WebVersionIntegralNumber.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        ApplicationContext db = new ApplicationContext(CreateOptions.JSON_FILE_Configuration());
+        public IActionResult Index()
+        {
+
+            IEnumerable<Number> query = from item in db.Numbers.OrderByDescending(key => key.NumberID)
+                                        where item.NumberID < 6
+                                        select item;
+            ViewBag.Numbers = query;
+            return View();
+        }
+        [HttpGet]
+
+        [HttpPost]
+        public ActionResult Result(int Number)
+        {
+            ControlInput num = new ControlInput();
+            if (!num.Check(Number.ToString())) return RedirectToAction("Index");
+            Number Input = new Number() { Num = Number };
+            Calculate Calculator = new Calculate();
+            Calculator.Squaring(Input);
+            db.Numbers.Add(Input);
+            db.SaveChanges();
+            ViewBag.Number = Input;
+            return View();
+        }
+
+        //public IActionResult Privacy()
+        //{
+        //    return View();
+        //}
+
+        //[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        //public IActionResult Error()
+        //{
+        //    return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        //}
+    }
+}
